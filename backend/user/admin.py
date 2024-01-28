8494fd2e39f3bd9520968009781d4cfbb76bac86
Ishan1Pant <ishanpant104@gmail.com>
2024-01-28 15:03:44 +0530

    Created custom user Model

---
+++ backend/user/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+class UserAdmin(UserAdmin):
+
+    list_display = ["email", "name", "is_admin"]
+    list_filter = ["is_admin","created_at"]
+    fieldsets = [
+        (None, {"fields": ["email", "password"]}),
+        ("Personal info", {"fields": ["name"]}),
+        ("Permissions", {"fields": ["is_admin"]}),
+    ]
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = [
+        (
+            None,
+            {
+                "classes": ["wide"],
+                "fields": ["email", "name", "password1", "password2"],
+            },
+        ),
+    ]
+    search_fields = ["email"]
+    ordering = ["email"]
+    filter_horizontal = []
+
+
+admin.site.register(User, UserAdmin)
